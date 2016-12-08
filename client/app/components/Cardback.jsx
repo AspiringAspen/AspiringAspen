af04a6a08fedecc5c310604005df3a1e0a83f244
Bruce Paul <brucewpaul@gmail.com>
2016-12-07 20:01:58 -0800

    change data structure and opps add userhand info

--- client/app/components/Cardback.jsx
+++ client/app/components/Cardback.jsx
@@ -1,2 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+const Card = (props) => {
+  return (
+    <div>
+      <p>Back of card</p>
+    </div>
+  )
+}
+
+Card.propTypes = {
+  cardInfo: PropTypes.object.isRequired,
+};
